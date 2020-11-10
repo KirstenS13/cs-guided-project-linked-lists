1684a03d17e4461665f3c8c1e23e74a48ebf0fda
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-11-09 19:46:37 -0500

    finished lecture

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -28,8 +28,17 @@ class LinkedListNode():
         self.value = value
         self.next  = None
 
+# this is a very tricky problem
 def delete_node(node_to_delete):
     # Your code here
+    # UNDERSTAND
+    # we don't have access to the head or tail, only to the node to delete
+
+    # PLAN
+    # set node_to_delete.value to node_to_delete.next.value
+    node_to_delete.value = node_to_delete.next.value
+    # set node_to_delete.next to node_to_delete.next.next
+    node_to_delete.next = node_to_delete.next.next
 
 
 x = LinkedListNode('X')
@@ -39,4 +48,15 @@ z = LinkedListNode('Z')
 x.next = y
 y.next = z
 
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
+
+print("-----------")
 delete_node(y)
+
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -13,27 +13,84 @@ once.
 running your function by hand. Does it actually work? Also, don't forget to
 consider edge cases (like a list with only 1 or 0 elements).*
 """
+
+# So basically we want the pointers pointing this way <-- not this way -->
+#   we're reversing the direction of the pointer, not the order of the list
+
 class LinkedListNode():
     def __init__(self, value):
         self.value = value
         self.next  = None
 
 def reverse(head_of_list):
+    # soln that was in here before lecture
+    # current_node = head_of_list
+    # previous_node = None
+    # next_node = None
+
+    # # Until we have 'fallen off' the end of the list
+    # while current_node:
+    #     # Copy a pointer to the next element
+    #     # before we overwrite current_node.next
+    #     next_node = current_node.next
+
+    #     # Reverse the 'next' pointer
+    #     current_node.next = previous_node
+
+    #     # Step forward in the list
+    #     previous_node = current_node
+    #     current_node = next_node
+
+    # return previous_node
+
+    # soln from lecture
+    # reverse all the pointers:
+    # loop (while) go while... we still have nodes to process
+    # we have a current_node
+    # initialize current_node
     current_node = head_of_list
-    previous_node = None
-    next_node = None
+    # we need a reference to prev
+    # when we start, prev is none
+    prev = None
 
-    # Until we have 'fallen off' the end of the list
-    while current_node:
-        # Copy a pointer to the next element
-        # before we overwrite current_node.next
+    # on each iteration of the loop:
+    while current_node is not None:
+        print("current node:", current_node.value)
+    # we need a reference to next_node:
+    # next_node = current_node.next
         next_node = current_node.next
-
-        # Reverse the 'next' pointer
-        current_node.next = previous_node
-
-        # Step forward in the list
-        previous_node = current_node
+    # set current_node.next to prev
+        current_node.next = prev
+    # set prev to cur_node
+        prev = current_node
+    # update current_node to point to next_node
         current_node = next_node
 
-    return previous_node
+    return current_node
+
+x = LinkedListNode('X')
+y = LinkedListNode('Y')
+z = LinkedListNode('Z')
+
+x.next = y
+y.next = z
+
+cur_node = x
+while cur_node:
+    print(cur_node.value)
+    cur_node = cur_node.next
+    
+print("------")
+new_head = reverse(x)
+print(new_head)
+#
+# print("reversed list:")
+# cur_node = new_head
+# while cur_node:
+#     print(cur_node.value)
+#     cur_node = cur_node.next
+
+# print("-----")
+# print(x.value, x.next)
+# print(y.value, y.next.value)
+# print(z.value, z.next)
